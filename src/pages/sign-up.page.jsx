a676fc5e6c842d75a782bf788f5c2b5ac079d9ba
Imaadh Ifthikar <101471526+imaadh0@users.noreply.github.com>
2024-08-22 16:34:10 +0530

    Initial commit

---
+++ src/pages/sign-up.page.jsx
@@ -0,0 +1,10 @@
+import { SignUp } from "@clerk/clerk-react";
+
+function SignUpPage() {
+  return (
+  <div className="min-h-screen flex justify-center items-center">
+    <SignUp/>
+  </div>);
+}
+
+export default SignUpPage;
